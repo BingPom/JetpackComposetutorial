977b1c9098bb0c8b62340d962bf932c52c24519a
BingPom <bingpom@mailfence.com>
2023-10-17 12:08:12 +0200

    Finalizado

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Jetpack_page(getString(R.string.jetpack_paragraph_2))
                 }
             }
         }
@@ -30,17 +41,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Jetpack_page(name: String, modifier: Modifier = Modifier) {
+    val imagen = painterResource(id = R.drawable.bg_compose_background)
+    Column(
+        verticalArrangement = Arrangement.Top,
         modifier = modifier
+                .fillMaxSize()
     )
+    {
+        Image(
+            painter = imagen,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = stringResource(id = R.string.jetpack_title),
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.jetpack_paragraph_1),
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp
+                    )
+        )
+        Text(
+            text = stringResource(id = R.string.jetpack_paragraph_1),
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeTutorialTheme {
-        Greeting("Android")
+        Jetpack_page("Android")
     }
 }
